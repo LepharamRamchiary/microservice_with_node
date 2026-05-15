bb1f7d1e47f86bf704d4e8fe5e1b7a3fc20674e8
LepharamRamchiary <lepharamchiary@gmail.com>
2026-05-15 14:08:34 +0530

    added delete endpoint also

--- product/src/router/product.route.js
+++ product/src/router/product.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {getProductById, getProducts, createProduct} from "../controllers/product.controller.js";
+import {getProductById, getProducts, createProduct, deleteProduct} from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.route("/").post(createProduct);
 router.route("/").get(getProducts);
+router.route("/:id").delete(deleteProduct); 
 router.route("/:id").get(getProductById);
 
 export default router;

68037c1da52a86ddbe102843f7810ac593a5093b
LepharamRamchiary <lepharamchiary@gmail.com>
2026-05-25 20:13:04 +0530

    not working

--- auth/src/routes/user.routes.js
+++ auth/src/routes/user.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { register, login, verifyToken } from "../controllers/auth.controller.js";
+import {
+  register,
+  login,
+  verifyToken,
+  getMyProfile,
+} from "../controllers/auth.controller.js";
+import { authenticate } from "../../../api-gateway/src/middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register-user").post(register);
 router.route("/login").post(login);
 router.route("/verify").get(verifyToken);
+
+// protected route
+router.route("/me").get(authenticate, getMyProfile);
 
 export default router;

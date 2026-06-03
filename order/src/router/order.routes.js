ad643343f9be34d320e79bfe87c12533cb57e8f6
LepharamRamchiary <lepharamchiary@gmail.com>
2026-06-03 21:14:22 +0530

    feat: add updateOrderStatus functionality and update routes to include it

--- order/src/router/order.routes.js
+++ order/src/router/order.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createOrder, getMyOrders, getOrderById } from "../controllers/order.controllers.js";
+import { createOrder, getMyOrders, getOrderById, updateOrderStatus } from "../controllers/order.controllers.js";
 
 const router = Router();
 
 router.route("/").post(createOrder).get(getMyOrders);
 router.route("/:id").get(getOrderById);
+router.route("/:id/status").put(updateOrderStatus);
 
 export default router;
